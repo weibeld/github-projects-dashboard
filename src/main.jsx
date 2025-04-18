3f70904d265be79173ba7941a914249171b9b0fd
Daniel Weibel <danielmweibel@gmail.com>
2025-04-18 16:52:11 +0700

    Attempt at fixing rendering of application after auth (ChatGPT)

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,28 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ClerkProvider } from "@clerk/clerk-react";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
 import App from "./App";
+
+const clerkPublishableKey = "pk_test_ZGlyZWN0LWJvbmVmaXNoLTkyLmNsZXJrLmFjY291bnRzLmRldiQ";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ClerkProvider
-    publishableKey="pk_test_ZGlyZWN0LWJvbmVmaXNoLTkyLmNsZXJrLmFjY291bnRzLmRldiQ"
+    publishableKey={clerkPublishableKey}
     afterSignInUrl="/github-projects-dashboard"
     afterSignUpUrl="/github-projects-dashboard"
   >
-    <App />
+    <SignedIn>
+      <App />
+    </SignedIn>
+    <SignedOut>
+      <RedirectToSignIn />
+    </SignedOut>
   </ClerkProvider>
 );
+
