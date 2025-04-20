60edf0d255fd19f7ba0c5cb5253f51053cfc5271
Daniel Weibel <danielmweibel@gmail.com>
2025-04-21 02:25:29 +0700

    Build starter Svelte application (output in docs/)

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/github-projects-dashboard/',
   plugins: [svelte()],
+  build: {
+    // Output directory for GitHub Pages (temporary)
+    outDir: path.resolve(__dirname, '../docs'),
+    emptyOutDir: true,
+  },
 })
