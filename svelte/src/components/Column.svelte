902fc954c0e97274f8321f009788a5f48675cce7
Daniel Weibel <danielmweibel@gmail.com>
2025-04-25 17:53:36 +0700

    Add count to column headers (NOT YET WORKING)
    Count is currently static and does not update when moving projects
    between columns.

--- svelte/src/components/Column.svelte
+++ svelte/src/components/Column.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="bg-gray-100 p-3 rounded shadow flex flex-col">
-  <h2 class="text-lg font-semibold mb-2 capitalize">{title}</h2>
+  <h2 class="text-lg text-left font-semibold mb-2 capitalize">{title} <span class="text-gray-500 text-sm">({projects.length})</span></h2>
   <div class="space-y-2 min-h-[50px]" bind:this={containerRef}>
     {#each projects as project (project.id)}
       <ProjectCard {project} />
