04e0e7730d4df494b0cd2685e38dc19b86c7d4ed
Daniel Weibel <danielmweibel@gmail.com>
2025-05-01 18:49:34 +0200

    Implement dropdown menu
    - Emulates GitHub menu as much as possible
    - Menu slides in from the right on opening and disappears immediately on
      closing (like on GitHub)
    - When the menu opens, a shaded overlay is added to the entire screen
      and scrolling of the page is disabled (like on GitHub)
      - This makes the 'clickOutside.js' helper obsolete as now detecting
        a click on the overlay is enough to close the menu
    - The menu can also be closed with the Esc key

--- svelte/src/components/Header.svelte
+++ svelte/src/components/Header.svelte
@@ -1,18 +1,37 @@
 <script>
   export let session;
   export let onLogout;
-  import { Github, Book, User, PanelsTopLeft } from 'lucide-svelte';
+  import { fade } from 'svelte/transition';
+  import { Github, Book, User, PanelsTopLeft, X, LogOut, Hourglass, Code} from 'lucide-svelte';
   import logoUrl from '../../public/logo.svg?url';
-  import { clickOutside } from '../lib/clickOutside.js'
+  import { onMount, onDestroy } from 'svelte';
 
   let menuOpen = false;
-  const handleClickOutside = () => {
-    console.log("Clicked outside");
-    menuOpen = false;
-  };
+
+  // Disable scrolling when menu is open
+  $: {
+    if (menuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+  // Allow closing menu with Esc
+  onMount(() => {
+    const handleKeydown = (e) => {
+      if (e.key === 'Escape') {
+        menuOpen = false;
+      }
+    };
+    window.addEventListener('keydown', handleKeydown);
+    onDestroy(() => {
+      window.removeEventListener('keydown', handleKeydown);
+    });
+  });
 
   $: user = session?.user ?? null;
   $: ghUsername = user?.user_metadata?.user_name;
+  $: ghFullName = user?.user_metadata?.full_name;
   $: ghAvatarUrl = user?.user_metadata?.avatar_url;
   $: ghProfileUrl = `https://github.com/${ghUsername}`;
   $: ghProjectsUrl = `${ghProfileUrl}?tab=projects`;
@@ -25,7 +44,7 @@
   </div>
 
   {#if session}
-    <div class="flex items-center gap-3" use:clickOutside={handleClickOutside}>
+    <div class="flex items-center gap-3">
       <div class="flex items-center text gap-1">
         <a href="https://github.com" target="_blank" class="_box-link "><Github class="_icon" />GitHub</a>/
         <a href={ghProfileUrl} target="_blank" class="_box-link "><User class="_icon" />{ghUsername}</a>/
@@ -36,10 +55,49 @@
       </button>
 
       {#if menuOpen}
-        <div class="fixed top-0 right-0 mt-16 mr-4 w-60 bg-white rounded-md shadow-xl z-50 animate-slide-in">
-          <a href={ghProfileUrl} target="_blank" on:click={() => menuOpen = false} class="block px-4 py-2 hover:bg-githubButtonHoverBgColor">GitHub Profile</a>
-          <a href={ghProjectsUrl} target="_blank" on:click={() => menuOpen = false} class="block px-4 py-2 hover:bg-githubButtonHoverBgColor">GitHub Projects</a>
-          <button on:click={() => {menuOpen = false; onLogout();}} class="block w-full text-left px-4 py-2 hover:bg-githubRedHoverBgColor hover:text-githubRedColor">Logout</button>
+        <!-- Overlay when opening menu -->
+        <div class="fixed inset-0 bg-githubSecondaryTextColor bg-opacity-30 z-40" on:click={() => (menuOpen = false)} in:fade={{ duration: 150 }} out:fade={{ duration: 0 }}></div>
+        <!-- Menu container -->
+        <div class="fixed top-0 right-0 w-80 bg-white rounded-l-xl shadow-xl z-50 animate-slide-in-right overflow-hidden p-4">
+          <!-- Menu header -->
+          <div class="flex items-center justify-between mb-4 relative">
+            <!-- Left side of menu header -->
+            <div class="flex items-center gap-2">
+              <img src={ghAvatarUrl} alt="Avatar" class="w-9 h-9 rounded-full border border-githubBorderColor" />
+              <div class="flex flex-col text-sm leading-tight">
+                <span class="font-bold text-githubPrimaryTextColor">{ghUsername}</span>
+                <span class="text-githubSecondaryTextColor">{ghFullName}</span>
+              </div>
+            </div>
+            <!-- Right side of menu header (close button) -->
+            <button on:click={() => (menuOpen = false)} aria-label="Close menu" class="text-githubSecondaryTextColor hover:bg-githubButtonHoverBgColor p-2.5 rounded-lg absolute top-0 right-0 ">
+              <X class="_icon" />
+            </button>
+          </div>
+          <!-- Menu items -->
+          <div>
+            <a href="https://github.com/weibeld/github-projects-dashboard" target="_blank" on:click={() => menuOpen = false} class="_box-link p-2 gap-2 mb-1">
+              <Book class="_icon" />
+              Docs
+            </a>
+            <a href="https://github.com/weibeld/github-projects-dashboard" target="_blank" on:click={() => menuOpen = false} class="_box-link p-2 gap-2 mb-1">
+              <Code class="_icon" />
+              Code
+            </a>
+            <a href="javascript:void(0)" on:click={() => {menuOpen = false; onLogout();}} class="_box-link p-2 gap-2 mb-1">
+              <Hourglass class="_icon" />
+              Log out (temporary)
+            </a>
+            <hr class="border-t border-1 border-githubDividerColor mt-2 mb-2" />
+            <div class="flex items-center p-2 pb-1 gap-2 text-githubSecondaryTextColor">
+              <div>
+                <LogOut class="_icon" />
+              </div>
+              <div class="text-xs leading-tight text-githubSecondaryTextColor p-0">
+                To sign out, please log out from <a href="https://github.com/" target="_blank" class="_classic-link text-githubActionColor" on:click={() => menuOpen = false}>GitHub</a> in this browser.
+              </div>
+            </div>
+          </div>
         </div>
       {/if}
     </div>
