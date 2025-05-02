8d9030afca14204f7c4d24f7c2c6d12faed4fdb4
Daniel Weibel <danielmweibel@gmail.com>
2025-05-02 17:27:55 +0200

    Make Closed column in sliding pane stretch to the edges
    So that it has the same vertical positioning as the other columns.

--- svelte/src/components/ClosedColumnPane.svelte
+++ svelte/src/components/ClosedColumnPane.svelte
@@ -15,10 +15,10 @@
 
 <!-- Pane -->
 <div use:onEscape={onClose} use:disableScroll class="_slide-in-container top-16 w-1/2 h-[calc(100vh-4rem)] flex flex-col">
-  <button on:click={onClose} class="text-gray-500 hover:text-gray-700 p-2 rounded hover:bg-gray-300">
+  <!--<button on:click={onClose} class="text-gray-500 hover:text-gray-700 p-2 rounded hover:bg-gray-300">
     <X class="w-4 h-4" />
-  </button>
-  <div class="flex-1 overflow-y-auto p-4">
+  </button>-->
+  <div class="flex-1 w-full grid grid-cols-1 sm:grid-cols-1 pt-4 pb-2 px-4">
     <Column title="closed" projects={projects} readonly={true} />
   </div>
 </div>
