55ba773f1e0788c4c1955df34f6c9b7edbb0024b
Daniel Weibel <danielmweibel@gmail.com>
2025-09-13 19:02:54 +0200

    Add connection strings for new Supabase project
    Previous Supabase project has been paused and expired to to inactivity.

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   // Find in Supabase project dashboard > Project Settings > Data API
-  'https://ptmcrrrrdsjgzfdawzkg.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0bWNycnJyZHNqZ3pmZGF3emtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMDI4MzYsImV4cCI6MjA2MDU3ODgzNn0.E_jnXoiniR8-X_VBw3BebpeuBCSTGtZb5qgzMONcvDI'
+  'https://hseukcyyqsgfjrosdhyp.supabase.co',
+  // Find in Supabase project dashboard > Project Settings > API Keys
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzZXVrY3l5cXNnZmpyb3NkaHlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc3ODA1NDcsImV4cCI6MjA3MzM1NjU0N30.YdPJBtwsp4pf3SiwK_e1jYAYc0PzT8vKX2VE1yr2EAE'
 );
